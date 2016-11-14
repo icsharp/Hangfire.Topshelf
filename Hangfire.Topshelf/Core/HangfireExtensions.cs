fd520925ec09f729d1cf03167f39027e392179bc
icsharp <554351220@qq.com>
2016-11-14 16:21:27 +0800

    add comments on code.

--- Hangfire.Topshelf/Core/HangfireExtensions.cs
+++ Hangfire.Topshelf/Core/HangfireExtensions.cs
@@ -113,11 +113,23 @@ namespace Hangfire.Topshelf.Core
 			return container;
 		}
 
+		/// <summary>
+		/// Build <see cref="RecurringJob"/> automatically within specified interface or class.
+		/// </summary>
+		/// <param name="configuration"><see cref="IGlobalConfiguration"/></param>
+		/// <param name="types">Specified interface or class</param>
+		/// <returns><see cref="IGlobalConfiguration"/></returns>
 		public static IGlobalConfiguration UseRecurringJob(this IGlobalConfiguration configuration, params Type[] types)
 		{
 			return UseRecurringJob(configuration, () => types);
 		}
 
+		/// <summary>
+		/// Build <see cref="RecurringJob"/> automatically within specified interface or class.
+		/// </summary>
+		/// <param name="configuration"><see cref="IGlobalConfiguration"/></param>
+		/// <param name="typesProvider">The provider to get specified interfaces or class.</param>
+		/// <returns><see cref="IGlobalConfiguration"/></returns>
 		public static IGlobalConfiguration UseRecurringJob(this IGlobalConfiguration configuration, Func<IEnumerable<Type>> typesProvider)
 		{
 			if (typesProvider == null) throw new ArgumentNullException(nameof(typesProvider));
